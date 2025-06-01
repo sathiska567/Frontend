09bf7671136b70da9c7b0d9c84a6bec3854eff26
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-06-02 01:21:48 +0530

    added

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useAuth } from "./auth/AuthContext";
+import { useAuth } from "./Auth/AuthContext";
 import { checkAuth } from "../store/slices/authSlice";
 import authService from "../services/authService";
 
